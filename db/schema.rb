148dc629ea5281007aec177dca4107a47d58c335
zdebyman <yuriy.zdebskyi@gmail.com>
2024-04-16 22:41:33 -0400

    Added notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_003106) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_004942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_003106) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notes", "users"
 end
